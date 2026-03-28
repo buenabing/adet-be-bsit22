eacd3fd9d37fac9c6aa1fa51fe2566e3eefbf14c
xessuu <bmabing62681@liceo.edu.ph>
2026-03-28 14:07:03 +0800

    activity

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # adet-be-bsit22
+```
+npm install
+npm run dev
+```
+
+```
+open http://localhost:3000
+```
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import postRoute from './routes/post.route.js';
+
+const app = new Hono()
+
+app.get('/', (c) => { return c.text('Hello Hono!')
+})
+
+
+app.route('/post', postRoute);
+
+serve({ fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
